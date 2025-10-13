cf4b152b36c3c89298c6fea5ad8e30311d8a6212
arraylas <122151430+arraylas@users.noreply.github.com>
2025-10-13 15:09:27 +0000

    minikit integration

--- del.sh
+++
@@ -1,8 +0,0 @@
-# Pastikan node_modules dihapus dari index dan di-ignore
-echo "node_modules/" >> .gitignore
-
-# Gunakan filter-repo untuk bersih-bersih riwayat besar
-pip install git-filter-repo
-
-# Hapus semua file node_modules dari seluruh riwayat Git
-git filter-repo --path node_modules --invert-paths --force
---
+++ minikit.config.ts
@@ -0,0 +1,24 @@
+export default {
+  app: {
+    name: "Farcaster Achievement",
+    description: "Generate achievement image from your Farcaster history",
+    icon: "https://farcaster-achivement.vercel.app/icon.png",
+    url: "https://farcaster-achivement.vercel.app",
+  },
+  accountAssociation: {
+    header: "eyJmaWQiOjI5Mjg4MywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDE0QzZBM0ZiMGZhNDZBNDNkMDFEQzc0MjZEN2MyN2NmOEM3MWMxMGEifQ",
+    payload: "eyJkb21haW4iOiJmYXJjYXN0ZXItYWNoaXZlbWVudC52ZXJlbC5hcHAifQ",
+    signature: "MHhhZDBkMjFiODNjYjdhMWZjNTA1ZWRiOTUzYTczYjFlMzExMTUwMWIyMjJmMDkzOTNkMjk4Mjg4NDI2NjVhMzkyNTQ0YWIzMDM0NmViMDJhZGVkOGE2NWViYTQ4OThlODE0ZDgzN2EwNzE4YjcwNDNlYTlmNmZkY2IzMDIyNDRiODFi"
+  },
+  frame: {
+    version: "1",
+    name: "Example Frame",
+    iconUrl: "https://farcaster-achivement.vercel.app/icon.png",
+    homeUrl: "https://farcaster-achivement.vercel.app",
+    imageUrl: "https://farcaster-achivement.vercel.app/image.png",
+    buttonTitle: "Check this out",
+    splashImageUrl: "https://farcaster-achivement.vercel.app/splash.png",
+    splashBackgroundColor: "#eeccff",
+    webhookUrl: "https://farcaster-achivement.vercel.app/api/webhook"
+  }
+};
